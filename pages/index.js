2a5d36590692bab5c9cd4769e6a4b505aeddc260
MonokaiJs <monokaijs@gmail.com>
2023-02-14 13:47:20 +0700

    add some nextjs files

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function HomePage() {
+  return (
+    <div>
+      Welcome to ChatGPT Bot. Application has started properly, please head to Telegram Bot and send some message!
+    </div>
+  )
+}
