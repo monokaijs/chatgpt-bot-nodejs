93651e1e5071a40c1abb6ff0455353117d019e1c
MonokaiJs <monokaijs@gmail.com>
2023-02-14 14:37:18 +0700

    add logging for vercel prod debug

--- pages/api/telegram-webhook.js
+++ pages/api/telegram-webhook.js
@@ -23,6 +23,7 @@ export default async function handler(req, res) {
   } else {
     const {body} = req;
     const msg = body.message;
+    console.log(msg);
     await DbService.connect();
     TelegramService.register();
     await TelegramService.responseToMessage(msg);
