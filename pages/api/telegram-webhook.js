710ed281c8f92bd992db104dff05850a04e03ff8
MonokaiJs <monokaijs@gmail.com>
2023-02-15 20:19:53 +0700

    :bug: change getting webhook url behavior

--- pages/api/telegram-webhook.js
+++ pages/api/telegram-webhook.js
@@ -3,19 +3,15 @@ import TelegramService from "../../services/telegram.service";
 
 export default async function handler(req, res) {
   const vercelUrl = process.env.VERCEL_URL;
-  const webhookPath = `https://${vercelUrl}/api/telegram-webhook`;
+  const webhookPath = vercelUrl ? `https://${vercelUrl}/api/telegram-webhook` : `https://${req.headers.host}${req.url}`;
   if (req.method === 'GET') {
-    if (!vercelUrl) {
-      return res.json({
-        message: 'Please deploy this project to Vercel first'
-      });
-    }
     TelegramService.register();
     TelegramService.bot.setWebHook(webhookPath).then(() => {
       res.json({
         message: 'Telegram Webhook has been successfully set'
       });
     }).catch((e) => {
+      console.log(e);
       res.json({
         message: 'Failed to setup Telegram Webhook'
       })
